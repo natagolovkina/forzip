59a82b49d43534837cb0705800fafc00ea3cdf78
DenSpar <vorobev.denis@list.ru>
2020-03-27 11:05:10 +0300

    вчера забыл запушить 1) добавил функционал для кнопки оставить заявку, чтобы ее не было видно вверху экрана, но появлялась при скроллинге вниз на высоту строки меню. для этого добавил js файл. 2) заменил лого на белый на темном фоне и увеличил его, т.к. высота шапки ранее увеличивали 3) удалил папку с картинками для кнопки поиска, т.к. от кнопки отказались

---
+++ navbarOnlineRequest.js
@@ -0,0 +1,15 @@
+let btnOnlineRequest = document.querySelector('#btnOnlineRequest')
+//
+function showBtnOnlineRequest() {
+  if (window.pageYOffset > 50) {
+    btnOnlineRequest.classList.remove('hidden')
+  } else { btnOnlineRequest.classList.add('hidden') }
+}
+//
+/*
+btnOnlineRequest.onclick = function () {
+	//написать сюда ссылку перехода на страницу заявки
+}
+*/
+// When scrolling, we run the function
+window.onscroll = showBtnOnlineRequest;
