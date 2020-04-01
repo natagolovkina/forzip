a7f7b1d039b350e667d499f8ec5fdc8897648437
DenSpar <vorobev.denis@list.ru>
2020-04-01 13:05:01 +0300

    в карусели заменил картинки на карточки

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,9 +1,9 @@
-let step = 147; // ширина элемента, шаг сдвига карусели
+let step = 168; // ширина элемента, шаг сдвига карусели
 let count = 3; // видимое количество изображений
 
 let carousel = document.querySelector('#carouselGoods');
 let list = carousel.querySelector('.content__goodsCarousel_gallery');
-let listElems = carousel.querySelectorAll('.content__goodsCarousel_gallery_item');
+let listElems = carousel.querySelectorAll('.content__goodsCarousel_gallery_cardContainer');
 
 let position = 0; // положение ленты прокрутки
 
@@ -29,4 +29,4 @@ setInterval(function() {
 	position = Math.max(position, -step * (listElems.length - count));	
   };
   list.style.marginLeft = position + 'px';
-},2000);
+},3000);
