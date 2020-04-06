77e3bbc4826a93450eeb936a7b683d44205c3c47
DenSpar <vorobev.denis@list.ru>
2020-04-06 19:25:53 +0300

    сделал автозаполнение карусели на главной

--- main/carousel/carousel.js
+++ main/carousel/carousel.js
@@ -1,3 +1,30 @@
+var carouselItems = [
+  {name:"Компрессор Зубр", imgCarousel:"main/carousel/1.png"},
+  {name:"Ресивер AIRRUS (РКЗ)", imgCarousel:"main/carousel/2.png"},
+  {name:"Части для компрессоров", imgCarousel:"main/carousel/3.png"},
+  {name:"Гидрометр", imgCarousel:"main/carousel/4.png"},
+  {name:"Колесо шасси", imgCarousel:"main/carousel/5.png"},
+];
+
+var carouselGallery = document.querySelector('#main-carouselGallery');
+var carouselCardTemplateContent = carouselGallery.querySelector('#main-carouselCard').content;
+var carouselCardTemplate = carouselCardTemplateContent.querySelector('.content__goodsCarousel_gallery_cardContainer');
+
+var makeNewItemForCarousel = function (src, name) {
+    let newCard = carouselCardTemplate.cloneNode(true);
+    let newImg = newCard.querySelector('.carouselCard_img');
+    newImg.src = src;
+    let newName = newCard.querySelector('.carouselCard_name');
+    newName.textContent = name;
+    carouselGallery.appendChild(newCard);
+};
+
+for (i = 0; i < carouselItems.length; i++) {
+  let imgPath = carouselItems[i].imgCarousel;
+  let title = carouselItems[i].name;
+  makeNewItemForCarousel(imgPath,title);
+}; 
+
 let step = 172; // ширина элемента, шаг сдвига карусели
 let count = 3; // видимое количество изображений
 
