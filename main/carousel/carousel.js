3738d11ff73c828a573d8189f4aac8b9b6df24fb
DenSpar <vorobev.denis@list.ru>
2020-04-15 16:29:16 +0300

    переделал класс кнопки ОСТАВИТЬ ЗАЯВКУ на visibility:hidden нашел-исправил ошибку в шаблоне карточки карусели

--- main/carousel/carousel.js
+++ main/carousel/carousel.js
@@ -3,7 +3,7 @@ let carouselItems = [
   {name:"Ресивер AIRRUS (РКЗ)", imgCarousel:"main/carousel/2.png"},
   {name:"Части для компрессоров", imgCarousel:"main/carousel/3.png"},
   {name:"Гидрометр", imgCarousel:"main/carousel/4.png"},
-  {name:"Колесо шасси", imgCarousel:"main/carousel/5.png"},
+  {name:"Колесо шасси", imgCarousel:"main/carousel/5.png"}
 ];
 
 let carouselBox = document.querySelector('#main-carouselBox');
