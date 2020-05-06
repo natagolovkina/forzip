7efcd39bbe440475820e00462735156b26a96895
DenSpar <vorobev.denis@list.ru>
2020-05-06 17:01:49 +0300

    сделал одинаковые паддинги в блоках и перенес их[паддинги] в innerContainer

--- common/menu/menuLoader.js
+++ common/menu/menuLoader.js
@@ -1,6 +1,6 @@
 let menuMainContainer = document.querySelector('#menu-place');
 
-let newMenuItem = function (str,href,id) {
+let newMenuItem = function (str,href,id,order) {
     let aItem = document.createElement('a');
     aItem.classList.add('menu__list_itemText','text-decoration_none');
     aItem.href = href;
@@ -8,15 +8,18 @@ let newMenuItem = function (str,href,id) {
     aItem.textContent = str;
     let liItem = document.createElement('li');
     liItem.classList.add('menu__list_item');
-    if (str === 'Online заявка') {
-        liItem.classList.add('margin-left_auto');
+    if (order === 'first') {
+        liItem.classList.add('menu__list_firstItem');
+    };
+    if (order === 'last') {
+        liItem.classList.add('menu__list_lastItem');
     };
     liItem.append(aItem);
     menuMainContainer.append(liItem);
 };
 
 //Главная
-newMenuItem('Главная','HTML main.html','menu-main');
+newMenuItem('Главная','HTML main.html','menu-main','first');
 
 //Каталог
 newMenuItem('Каталог','HTML catalog.html','menu-catalog');
@@ -28,4 +31,4 @@ newMenuItem('Обслуживание','HTML service.html','menu-service');
 newMenuItem('Контакты','HTML contacts.html','menu-contacts');
 
 //Online заявка
-newMenuItem('Online заявка','HTML request.html','menu-request');
+newMenuItem('Online заявка','HTML request.html','menu-request','last');
