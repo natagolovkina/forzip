99ef332f5f96236bf1d4abddadb7380ded153098
DenSpar <vorobev.denis@list.ru>
2020-05-13 17:29:48 +0300

    перенес заглушки для страницы каталога в папку catalog

--- catalog/catalog-sectionCards.js
+++ catalog/catalog-sectionCards.js
@@ -1,16 +1,16 @@
 let cardsArr = [
-    {name:"Винтовые компрессоры", img:"stubs/catalogStubs/ScrewСompressor_180x175.jpg"},
-    {name:"Поршневые компрессоры", img:"stubs/catalogStubs/pistonСompressor_180x180.jpg"},
-    {name:"Дизельные компрессоры", img:"stubs/catalogStubs/dieselСompressor_180x180.jpg"},
-    {name:"Безмасляные компрессоры", img:"stubs/catalogStubs/oil-freeСompress_180x180.jpg"},
-    {name:"Системы подготовки сжатого воздуха", img:"stubs/catalogStubs/CompressedAirPrepSystems_180x180.jpg"},
-    {name:"Магистральные фильтры", img:"stubs/catalogStubs/MainFilter_180x180.jpg"},
-    {name:"Ресиверы", img:"stubs/catalogStubs/resivers_180x180.jpg"},
-    {name:"Расходники (фильтры масляные, сепараторы, воздушные фильтры, масла)", img:"stubs/catalogStubs/Consumables_179x180.jpg"},
-    {name:"Запасные части для винтовых компрессоров", img:"stubs/catalogStubs/SpareParts_180x180.jpg"},    
-    {name:"Запасные части для поршневых компрессоров", img:"stubs/catalogStubs/SpareParts_180x180.jpg"},
-    {name:"Реле давления", img:"stubs/catalogStubs/PressureSwitch_180x180.jpg"},
-    {name:"Масла и смазки", img:"stubs/catalogStubs/OilsAndLubricants.jpg"}
+    {name:"Винтовые компрессоры", img:"catalog/catalogStubs/ScrewСompressor_180x175.jpg"},
+    {name:"Поршневые компрессоры", img:"catalog/catalogStubs/pistonСompressor_180x180.jpg"},
+    {name:"Дизельные компрессоры", img:"catalog/catalogStubs/dieselСompressor_180x180.jpg"},
+    {name:"Безмасляные компрессоры", img:"catalog/catalogStubs/oil-freeСompress_180x180.jpg"},
+    {name:"Системы подготовки сжатого воздуха", img:"catalog/catalogStubs/CompressedAirPrepSystems_180x180.jpg"},
+    {name:"Магистральные фильтры", img:"catalog/catalogStubs/MainFilter_180x180.jpg"},
+    {name:"Ресиверы", img:"catalog/catalogStubs/resivers_180x180.jpg"},
+    {name:"Расходники (фильтры масляные, сепараторы, воздушные фильтры, масла)", img:"catalog/catalogStubs/Consumables_179x180.jpg"},
+    {name:"Запасные части для винтовых компрессоров", img:"catalog/catalogStubs/SpareParts_180x180.jpg"},    
+    {name:"Запасные части для поршневых компрессоров", img:"catalog/catalogStubs/SpareParts_180x180.jpg"},
+    {name:"Реле давления", img:"catalog/catalogStubs/PressureSwitch_180x180.jpg"},
+    {name:"Масла и смазки", img:"catalog/catalogStubs/OilsAndLubricants.jpg"}
 ];
 
 let cardsBox = document.querySelector('#catalog-sectionCards');
