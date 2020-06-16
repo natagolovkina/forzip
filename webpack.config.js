ad302f6faab7580d42b9af8fb43ad26f8bb6abb7
DenSpar <vorobev.denis@list.ru>
2020-06-16 12:45:51 +0300

    установил webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -26,29 +27,8 @@ const optimization = () => {
     return config
 };
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        main:'./index.js'
-    },    
-    output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js', '.json'],
-        alias: {
-            '@js': path.resolve(__dirname, 'src/js'),
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    optimization: optimization(),
-    devServer: {
-        port: 4200,
-        hot: isDev
-    },
-    plugins: [
+const plugins = () => {
+    const base = [
         new HTMLWebpackPlugin({
             template: './assets/template.html',
             collapseWhitespace: isProd
@@ -73,7 +53,38 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: filename('css')
         })
-    ],
+    ];
+
+    if (isProd) {
+        base.push(new BundleAnalyzerPlugin())
+    };
+
+    return base
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './index.js'],
+    },    
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+        alias: {
+            '@js': path.resolve(__dirname, 'src/js'),
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: plugins(),
     module: {
         rules: [
             {
@@ -96,6 +107,18 @@ module.exports = {
             {
                 test: /\.(ttf|woff|woff2|eot)$/,
                 use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
             }
         ]
     }
