887aadadede1400a27b132b83d8bf8d2bd003ea7
DenSpar <vorobev.denis@list.ru>
2020-08-04 18:56:37 +0300

    удалил из компонентов стили

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,58 +1,20 @@
 import React, {Fragment} from 'react';
 import brandsTest from '@/maps/brands/brandsMap';//тестовый массив с брендами
-
-const styles = {
-    content__brends: {
-        paddingTop: "40px",
-        flexWrap: "wrap"
-    },
-    content__brends_card: {
-        margin: "10px",
-        padding: "10px",
-        maxWidth: "100px",
-        borderColor: "#ced4da",
-        backgroundColor: "white",
-        backgroundSize: "cover",
-        cursor: "pointer"
-    },
-    /* ховер
-    content__brends_card:hover: {
-        boxShadow: "0 0 10px grey"
-    },*/
-    /*brendCard: {},*/
-    brendLogoContainer: {
-        height: "100px",
-        width: "100px"
-    },
-    brendLogo: {
-        display: "block",
-        margin: "0 auto",
-        maxHeight: "100px",
-        maxWidth: "100px"
-    },
-    brendName: {
-        marginTop: "10px"
-    }
-};
+import '@/styles/block-brands.css';
 
 export const Brands = ({brands}) => {
     brands = brandsTest;
     return(
-        <div className="content__brands margin_auto width_90 flex justify-content_space-around" id="main-brands"
-        style={styles.content__brends}>
+        <div className="content__brands margin_auto width_90 flex justify-content_space-around" id="main-brands">
             {brands.map((brand, i) => (
-                <div className="content__brends_card flex border_1px border-radius_12px"
-                style={styles.content__brends_card}
+                <div className="content__brands_card flex border_1px border-radius_12px"
                 key={i}>
-                    <figure className="brendCard flex flex-direction_column">
-                        <div className="brendLogoContainer flex align-items_center"
-                        style={styles.brendLogoContainer}>
+                    <figure className="brandCard flex flex-direction_column">
+                        <div className="brandLogoContainer flex align-items_center">
                             <img src={brand.img130}
-                            className="brendLogo"
-                            style={styles.brendLogo} />
+                            className="brandLogo" />
                         </div>
-                        <figcaption className="brendName cardName font-size_12px"
-                        style={styles.brendName}>
+                        <figcaption className="brandName cardName font-size_12px">
                             {brand.name}
                         </figcaption>
                     </figure>				
--- src/components/CarouselGoods.js
+++ src/components/CarouselGoods.js
@@ -1,59 +1,20 @@
 import React, {Fragment} from 'react';
 import carouselItemsTest from '@/maps/carousel/carouselMap';//тестовый массив с товарами
 import {CarouselItem} from '@/components/CarouselItem';
-
-const styles = {
-    content__containerGoods: {width: '65%'},
-    content__goodsCarousel: {
-        height: 'auto',
-        width: '100%',
-        position: 'relative'
-    },
-    content__goodsCarousel_arrow: {
-        height: '168px',
-        margin:' 0 5px',
-        padding: '5px',
-        fontFamily: 'sans-serif',
-        fontSize: '30px',
-        color: '#343a40',
-        borderColor: '#ced4da',
-    },
-    /*.content__goodsCarousel_arrow:focus {outline: none;}
-    .content__goodsCarousel_arrow:hover {cursor: pointer;}*/
-    prevCarousel: {left: '7px'},
-    nextCarousel: {right: '7px'},
-    content__goodsCarousel_galleryWindow: {
-        width: '510px',
-        overflow: 'hidden'
-    },
-    content__goodsCarousel_gallery: {
-        width: '9999px',
-        margin: '0',
-        padding: '0',
-        listStyle: 'none',
-        transition: 'margin-left 250ms',
-        fontSize: '0'
-    }
-};
+import '@/styles/block-carousel.css';
 
 export const CarouselGoods = () => {
     return(
         <Fragment>
-            <div className="content__containerGoods flex justify-content_center" 
-            style={styles.content__containerGoods}>
-                <div className="content__goodsCarousel flex align-items_center justify-content_center" id="main-carouselBox" 
-                style={styles.content__goodsCarousel}>
-                    <div className="content__goodsCarousel_arrow prevCarousel border_1px border-radius_4px flex align-items_center" 
-                    style={styles.content__goodsCarousel_arrow/*, styles.prevCarousel*/}>◄</div>
-                    <div className="content__goodsCarousel_galleryWindow" 
-                    style={styles.content__goodsCarousel_galleryWindow}>
-                        <div className="content__goodsCarousel_gallery flex" 
-                        style={styles.content__goodsCarousel_gallery}>
+            <div className="content__containerGoods flex justify-content_center" >
+                <div className="content__goodsCarousel flex align-items_center justify-content_center" id="main-carouselBox">
+                    <div className="content__goodsCarousel_arrow prevCarousel border_1px border-radius_4px flex align-items_center">◄</div>
+                    <div className="content__goodsCarousel_galleryWindow">
+                        <div className="content__goodsCarousel_gallery flex">
                         <CarouselItem items = {carouselItemsTest}/>
                         </div>
                     </div>
-                    <div className="content__goodsCarousel_arrow nextCarousel border_1px border-radius_4px flex align-items_center" 
-                    style={styles.content__goodsCarousel_arrow/*, styles.nextCarousel*/}>►</div>
+                    <div className="content__goodsCarousel_arrow nextCarousel border_1px border-radius_4px flex align-items_center">►</div>
                 </div>
             </div>
         </Fragment>
