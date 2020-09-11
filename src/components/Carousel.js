012e61d2d411a0492dd9963fe1cd670edc066609
DenSpar <vorobev.denis@list.ru>
2020-09-11 13:30:53 +0300

    сделал прокурутку карусели

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import '@styles/block-carousel.css';
 
 //тестовый массив с товарами
@@ -30,47 +30,71 @@ const CarouselItem = ({items}) => {
     )
 };
 
-//логика проккрутки карусели
-let step = 170; // ширина элемента, шаг сдвига карусели
-let count = 3; // видимое количество изображений
-let position = 0; // положение ленты прокрутки
-
-function clickPrevCarousel () {
-    // сдвиг влево
-    position += step;
-    position = Math.min(position, 0);
-    console.log('click prev, position:', position);
-};
-
-function clickNextCarousel () {
-    // сдвиг вправо
-    position -= step;      
-    position = Math.max(position, -step * (carouselItemsTest.length - count));
-    console.log('click next, position:', position);
-};
-
-setInterval(function() {
-    //автоматическая прокрутка
-	if(position ==  -step * (carouselItemsTest.length - count)) {
-	position = 0;
-	} else {
-	position -= step;
-	position = Math.max(position, -step * (carouselItemsTest.length - count));	
-  };
-  console.log('int next, position:', position);
-},3000);
-
 export const Carousel = () => {
+    const [carouselState, setCarouselState] = useState({
+        step: 170, // ширина элемента, шаг сдвига карусели
+        count: 3, // видимое количество изображений
+        position: 0, // положение ленты прокрутки
+        itemsArr: carouselItemsTest, // массив айтемов
+        intervalOn: false
+    });
+
+    function clickPrevCarousel () {
+        // сдвиг влево
+        let newPosition = carouselState.position + carouselState.step;
+        newPosition = Math.min(newPosition, 0);
+        setCarouselState(prevState => ({
+            ...prevState,
+            position: newPosition
+        }));
+    };
+
+    function clickNextCarousel () {
+        // сдвиг вправо
+        let newPosition = carouselState.position - carouselState.step;
+        newPosition = Math.max(newPosition, -carouselState.step * (carouselState.itemsArr.length - carouselState.count));
+        setCarouselState(prevState => ({
+            ...prevState,
+            position: newPosition
+        }));
+    };
+
+    if (!carouselState.intervalOn) {
+        // автоматическая прокрутка
+        setCarouselState(prevState => ({
+            ...prevState,
+            intervalOn: true
+        }));
+
+        let rebuildState = function (prevState) {
+            let newState = {...prevState};
+            if (newState.position ==  -newState.step * (newState.itemsArr.length - newState.count)) {
+                newState.position = 0;
+            } else {
+                let newPosition = newState.position - newState.step;                    
+                newPosition = Math.max(newPosition, -newState.step * (newState.itemsArr.length - newState.count));
+                newState.position = newPosition;
+            };
+            return newState
+        };
+
+        setInterval(function() {
+            setCarouselState(prevState => rebuildState(prevState))
+        },3000);
+    };    
+
     return(
         <div className="content__containerGoods flex justify-content_center" >
             <div className="content__goodsCarousel flex align-items_center justify-content_center">
-                <div className="content__goodsCarousel_arrow prevCarousel border_1px border-radius_4px flex align-items_center" onClick={clickPrevCarousel}>◄</div>
+                <div className="content__goodsCarousel_arrow prevCarousel border_1px border-radius_4px flex align-items_center"
+                onClick={clickPrevCarousel}>◄</div>
                 <div className="content__goodsCarousel_galleryWindow">
-                    <div className="content__goodsCarousel_gallery flex" style={{marginLeft: position + 'px'}}>
-                    <CarouselItem items = {carouselItemsTest}/>
+                    <div className="content__goodsCarousel_gallery flex" style={{marginLeft: carouselState.position + 'px'}}>
+                    <CarouselItem items = {carouselState.itemsArr}/>
                     </div>
                 </div>
-                <div className="content__goodsCarousel_arrow nextCarousel border_1px border-radius_4px flex align-items_center" onClick={clickNextCarousel}>►</div>
+                <div className="content__goodsCarousel_arrow nextCarousel border_1px border-radius_4px flex align-items_center"
+                onClick={clickNextCarousel}>►</div>
             </div>
         </div>
     )
